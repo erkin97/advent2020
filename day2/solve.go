de1005b76234a708bfd5c0edb2565801418a06de
erkin97 <matkaziev.97@gmail.com>
2020-12-07 15:38:53 +0900

    Solve day2

---
+++ day2/solve.go
@@ -0,0 +1,52 @@
+package day2
+
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+//Solve day1
+func Solve() (int, error) {
+	inputFile, err := os.Open("day2/input")
+	if err != nil {
+		return 0, err
+	}
+	defer inputFile.Close()
+
+	var tests []string
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		text := scanner.Text()
+		tests = append(tests, text)
+	}
+
+	// parse with regex
+	re := regexp.MustCompile("([0-9]+)-([0-9]+) ([a-z]): (.+)")
+	ans := 0
+	for i := 0; i < len(tests); i++ {
+		match := re.FindStringSubmatch(tests[i])
+		left, _ := strconv.ParseInt(match[1], 10, 32)
+		right, _ := strconv.ParseInt(match[2], 10, 32)
+		testRune := rune(match[3][0])
+		password := match[4]
+
+		for pos, char := range password {
+			if char == testRune {
+				if pos == int(left-1) {
+					left = -1
+				} else if pos == int(right-1) {
+					right = -1
+				}
+			}
+		}
+
+		if left*right < 0 {
+			ans++
+		}
+	}
+
+	return ans, nil
+}
--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"advent2020/day1"
+	"advent2020/day2"
 	"fmt"
 )
 
 func main() {
 	ans, err := day1.Solve()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(ans)
+	}
+
+	ans, err = day2.Solve()
 	if err != nil {
 		fmt.Println(err)
 	} else {
